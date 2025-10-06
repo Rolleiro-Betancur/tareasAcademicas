88cf0671bbaef81c8b333df17637366a35236ef9
rogerbetan201@gmail.com <Rolleiro-betancur>
2025-10-06 09:15:22 -0500

    Se crearon  los atributos y metodos necesarios

--- src/main/java/com/example/SpringtTareasAcademicasdi/entities/Task.java
+++ src/main/java/com/example/SpringtTareasAcademicasdi/entities/Task.java
@@ -1,4 +1,84 @@
 package com.example.SpringtTareasAcademicasdi.entities;
-
+import java.util.*;
 public class Task {
+
+    private Long id;
+    private String nombre;
+    private String correo;
+    private String titulo;
+    private String descripcion;
+    private Date fecha_entrega;
+    private String estado;
+
+
+    public Task() {
+    }
+
+    public Task(Long id, String nombre, String correo, String titulo, String descripcion, Date fecha_entrega, String estado) {
+        this.id = id;
+        this.nombre = nombre;
+        this.correo = correo;
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+        this.fecha_entrega = fecha_entrega;
+        this.estado = estado;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Date getFecha_entrega() {
+        return fecha_entrega;
+    }
+
+    public void setFecha_entrega(Date fecha_entrega) {
+        this.fecha_entrega = fecha_entrega;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+
 }
